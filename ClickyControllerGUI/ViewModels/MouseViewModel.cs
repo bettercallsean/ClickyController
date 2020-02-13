f890d5b48dcc8e0e9e5a4f944b79eab23e13e223
Sean <seanedwards97@gmail.com>
2020-02-13 20:07:08 +0000

    Create MouseViewModel.cs

---
+++ ClickyControllerGUI/ViewModels/MouseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ClickyController;
+using ClickyControllerGUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClickyControllerGUI.ViewModels
+{
+    class MouseViewModel : BaseViewModel
+    {
+        public int XPosition { get => Mouse.XPosition; }
+        public int YPosition { get => Mouse.YPosition; }
+        
+    }
+}
