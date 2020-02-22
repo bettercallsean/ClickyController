a123b3dca54a8d8bd2235010fd866a6a0260852a
Sean <seanedwards97@gmail.com>
2020-02-22 14:37:28 +0000

    Create ScriptExecuter.cs

---
+++ ClickyControllerGUI/ViewModels/ScriptExecuter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using ClickyController;
+using ClickyControllerGUI.ViewModels;
+using Newtonsoft.Json;
+
+namespace ClickyControllerGUI.Models
+{
+    public class ScriptExecuter : BaseViewModel
+    {
+
+        public ScriptExecuter()
+        {
+
+        }
+
+        public string[] ScriptReader(string scriptFilepath)
+        {
+            string[] commandArray;
+
+            try
+            {
+                commandArray = File.ReadAllLines(scriptFilepath);
+                return commandArray;
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+
+                return null;
+            }
+
+        }
+
+        public void ScriptRunner(string[] commandArray)
+        {
+            Dictionary<string, Action<string>> commandDictionary = new Dictionary<string, Action<string>>();
+        }
+
+
+
+
+        public void ScriptWriter()
+        {
+            // TODO: Create logic for writing script files in a way that can be easily read by the ScriptReader method
+        }
+
+            
+    }
+}
