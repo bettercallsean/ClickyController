203b4019a7ea3b5304ee3db1b0bddecee9d43d2e
Sean <seanedwards97@gmail.com>
2020-02-22 14:37:13 +0000

    Add functions for handling mouse events

--- ClickyControllerGUI/ViewModels/MouseViewModel.cs
+++ ClickyControllerGUI/ViewModels/MouseViewModel.cs
@@ -2,14 +2,55 @@
 using ClickyControllerGUI.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ClickyControllerGUI.ViewModels
 {
-    class MouseViewModel : BaseViewModel
+    public class MouseViewModel : BaseViewModel
     {
-        public int XPosition { get => Mouse.XPosition; }
-        public int YPosition { get => Mouse.YPosition; }
-        
+        public int XPosition => Mouse.XPosition;
+        public int YPosition => Mouse.YPosition;
+
+        public static void MouseClick(string buttonType)
+        {
+            switch (buttonType)
+            {
+                case "L":
+                    Mouse.LeftClick();
+                    break;
+                case "R":
+                    Mouse.RightClick();
+                    break;
+                case "M":
+                    Mouse.MiddleClick();
+                    break;
+                default:
+                    break;
+            } 
+        }
+
+        public static void MoveMouse(string coordinateString)
+        {
+            List<int> coordinates = coordinateString.Split(' ').Select(int.Parse).ToList();
+
+            Mouse.MoveMouse(coordinates[0], coordinates[1]);
+        }
+
+        public static void ScrollWheel(string direction)
+        {
+            switch(direction)
+            {
+                case "U":
+                    Mouse.WheelUp();
+                    break;
+                case "D":
+                    Mouse.WheelDown();
+                    break;
+                default:
+                    break;
+            }
+        }
+
     }
 }
