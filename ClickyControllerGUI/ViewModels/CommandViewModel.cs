a8cba501e4e73d1bf84961087612689b802b95d1
Sean <seanedwards97@gmail.com>
2020-02-24 20:47:07 +0000

    Create CommandViewModel.cs

---
+++ ClickyControllerGUI/ViewModels/CommandViewModel.cs
@@ -0,0 +1,28 @@
+﻿using ClickyControllerGUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClickyControllerGUI.ViewModels
+{
+    public class CommandViewModel : BaseViewModel
+    {
+        private Command _command = new Command();
+
+        public string GetCommandMethod
+        {
+            get => _command.Method;
+            set { _command.Method = value;  OnPropertyChanged(); }
+        }
+        public string GetCommandNamespace
+        {
+            get => _command.Namespace;
+            set { _command.Namespace = value; OnPropertyChanged(); }
+        }
+        public string GetCommandParameters
+        {
+            get => _command.Parameters;
+            set { _command.Parameters = value; OnPropertyChanged(); }
+        }
+    }
+}
