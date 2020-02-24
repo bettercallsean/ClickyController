d3381604f791dfd5d4864ee1a3bab5fe43820924
Sean <seanedwards97@gmail.com>
2020-02-24 20:00:25 +0000

    Create UtilityViewModel.cs

---
+++ ClickyControllerGUI/ViewModels/UtilityViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ClickyControllerGUI.ViewModels
+{
+    public class UtilityViewModel
+    {
+        public static void Wait(string seconds)
+        {
+            Thread.Sleep(int.Parse(seconds) * 1000);
+        }
+    }
+}
