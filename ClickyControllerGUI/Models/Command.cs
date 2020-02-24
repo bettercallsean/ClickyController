ddb6ec7cf6037782b6d347502a77768f19a8be75
Sean <seanedwards97@gmail.com>
2020-02-24 20:04:37 +0000

    Create Command.cs

---
+++ ClickyControllerGUI/Models/Command.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClickyControllerGUI.Models
+{
+    public class Command
+    {
+        public Command()
+        {
+
+        }
+
+        public string CommandData { get; set; }
+        public string CommandMethod { get; set; }
+        public string CommandNamespace { get; set; }
+    }
+}
