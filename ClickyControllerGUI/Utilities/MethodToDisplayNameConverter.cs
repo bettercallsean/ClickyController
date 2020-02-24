b43a34f04924d7df5307a05967ea35cef2ce12ea
Sean <seanedwards97@gmail.com>
2020-02-24 22:06:55 +0000

    Create MethodToDisplayNameConverter.cs

---
+++ ClickyControllerGUI/Utilities/MethodToDisplayNameConverter.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ClickyControllerGUI.Utilities
+{
+    public class MethodToDisplayNameConverter : IValueConverter
+    {
+        private readonly static Dictionary<string, Dictionary<string, string>> MethodToDisplayNameDictionary = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>>>(Resources.MethodsToListDisplayName);
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return MethodToDisplayNameDictionary["displayname"][value.ToString()];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return MethodToDisplayNameDictionary["methodname"][value.ToString()];
+        }
+    }
+}
