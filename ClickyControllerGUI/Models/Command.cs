17a7843c63e7e51bcd0ea10b146810565fc58138
Sean <seanedwards97@gmail.com>
2020-02-26 23:09:48 +0000

    Add Run method

--- ClickyControllerGUI/Models/Command.cs
+++ ClickyControllerGUI/Models/Command.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace ClickyControllerGUI.Models
@@ -13,5 +14,17 @@ namespace ClickyControllerGUI.Models
 
         public string Parameters { get; set; }
         public string Method { get; set; }
+
+        public void Run()
+        {
+            Type type = Type.GetType("ClickyControllerGUI.Models.Controller"); ;
+            MethodInfo method = type.GetMethod(Method);
+            object classObject = Activator.CreateInstance(type);
+
+            if (method.GetParameters().Length > 0)
+                method.Invoke(classObject, new object[] { Parameters });
+            else
+                method.Invoke(classObject, null);
+        }
     }
 }
