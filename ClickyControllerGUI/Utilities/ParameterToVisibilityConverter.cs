28e15f095c925102d16ed4b8e459fb22b095b272
Sean <seanedwards97@gmail.com>
2020-02-26 17:54:01 +0000

    Create ParameterToVisibilityConverter.cs

---
+++ ClickyControllerGUI/Utilities/ParameterToVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using ClickyControllerGUI.Models;
+
+namespace ClickyControllerGUI.Utilities
+{
+    class ParameterToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            if (((Command)value).Parameters == "")
+                return Visibility.Visible;
+            else
+                return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (((Command)value).Parameters != "")
+                return Visibility.Hidden;
+            else
+                return Visibility.Visible;
+        }
+    }
+}
