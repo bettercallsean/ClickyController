426faac459bcf7950cd249b3ac2669e2cacdfe2a
Sean <seanedwards97@gmail.com>
2020-02-29 16:35:08 +0000

    Change Controller Model to ViewModel

---
+++ ClickyControllerGUI/ViewModels/ControllerViewModel.cs
@@ -0,0 +1,82 @@
+﻿using ClickyController;
+using ClickyControllerGUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ClickyControllerGUI.ViewModels
+{
+    internal class ControllerViewModel
+    {
+        public ControllerViewModel()
+        {
+
+        }
+
+        public void LeftClick() => Mouse.LeftClick();
+
+        public void RightClick() => Mouse.RightClick();
+
+        public void MiddleClick() => Mouse.LeftClick();
+
+        public void LeftDown() => Mouse.LeftPress();
+
+        public void LeftUp() => Mouse.LeftRelease();
+
+        public void RightDown() => Mouse.RightPress();
+
+        public void RightUp() => Mouse.RightRelease();
+
+        public void MiddleDown() => Mouse.MiddleDown();
+
+        public void MiddleUp() => Mouse.MiddleRelease();
+
+        public void MoveMouse(string coordinates)
+        {
+            try
+            {
+                int[] mouseCoordinates = coordinates.Split(' ').Select(int.Parse).ToArray();
+                Mouse.MoveMouse(mouseCoordinates[0], mouseCoordinates[1]);
+            }
+            catch(FormatException)
+            {
+
+            }
+        }
+
+        public void Wait(string seconds)
+        {
+            try
+            {
+                ClickyController.Controller.Wait(int.Parse(seconds));
+            }
+            catch(FormatException)
+            {
+
+            }
+            
+        }
+
+        public void EnterText(string text) => Keyboard.EnterText(text);
+
+        public void PressKey(string key) => Keyboard.KeyPress(key);
+
+        public void KeyDown(string key) => Keyboard.KeyDown(key);
+
+        public void KeyUp(string key) => Keyboard.KeyRelease(key);
+
+        public void Run(Command command)
+        {
+            Type type = Type.GetType("ClickyControllerGUI.Models.Controller"); ;
+            MethodInfo method = type.GetMethod(command.Method);
+            object classObject = Activator.CreateInstance(type);
+
+            if (method.GetParameters().Length > 0)
+                method.Invoke(classObject, new object[] { command.Parameters });
+            else
+                method.Invoke(classObject, null);
+        }
+    }
+}
