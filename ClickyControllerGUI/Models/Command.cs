28fa18ee2d1fb1bbd5bf0726a74137e4ea9f3e7a
Sean <seanedwards97@gmail.com>
2020-02-29 16:32:37 +0000

    Remove Run command

--- ClickyControllerGUI/Models/Command.cs
+++ ClickyControllerGUI/Models/Command.cs
@@ -15,16 +15,5 @@ namespace ClickyControllerGUI.Models
         public string Parameters { get; set; }
         public string Method { get; set; }
 
-        public void Run()
-        {
-            Type type = Type.GetType("ClickyControllerGUI.Models.Controller"); ;
-            MethodInfo method = type.GetMethod(Method);
-            object classObject = Activator.CreateInstance(type);
-
-            if (method.GetParameters().Length > 0)
-                method.Invoke(classObject, new object[] { Parameters });
-            else
-                method.Invoke(classObject, null);
-        }
     }
 }
