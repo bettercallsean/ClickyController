e6ec0fba421c4700866cdaa374085a52eaccbd4a
Sean <seanedwards97@gmail.com>
2020-04-14 13:58:29 +0100

    Remove unused methods

--- ClickyControllerGUI/ViewModels/MouseViewModel.cs
+++ ClickyControllerGUI/ViewModels/MouseViewModel.cs
@@ -12,45 +12,5 @@ namespace ClickyControllerGUI.ViewModels
         public int XPosition => Mouse.XPosition;
         public int YPosition => Mouse.YPosition;
 
-        public static void MouseClick(string buttonType)
-        {
-            switch (buttonType)
-            {
-                case "L":
-                    Mouse.LeftClick();
-                    break;
-                case "R":
-                    Mouse.RightClick();
-                    break;
-                case "M":
-                    Mouse.MiddleClick();
-                    break;
-                default:
-                    break;
-            } 
-        }
-
-        public static void MoveMouse(string coordinateString)
-        {
-            List<int> coordinates = coordinateString.Split(' ').Select(int.Parse).ToList();
-
-            Mouse.MoveMouse(coordinates[0], coordinates[1], true);
-        }
-
-        public static void ScrollWheel(string direction)
-        {
-            switch(direction)
-            {
-                case "U":
-                    Mouse.WheelUp();
-                    break;
-                case "D":
-                    Mouse.WheelDown();
-                    break;
-                default:
-                    break;
-            }
-        }
-
     }
 }
