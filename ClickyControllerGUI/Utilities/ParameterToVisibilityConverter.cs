3d6ca6ed75243a15715ccfeda14a6533b64692f4
Sean <seanedwards97@gmail.com>
2020-04-17 00:09:20 +0100

    Change object type from Command to CommandViewModel

--- ClickyControllerGUI/Utilities/ParameterToVisibilityConverter.cs
+++ ClickyControllerGUI/Utilities/ParameterToVisibilityConverter.cs
@@ -4,7 +4,7 @@ using System.Text;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
-using ClickyControllerGUI.Models;
+using ClickyControllerGUI.ViewModels;
 
 namespace ClickyControllerGUI.Utilities
 {
@@ -12,12 +12,12 @@ namespace ClickyControllerGUI.Utilities
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return ((Command)value).Parameters != null ? Visibility.Visible : Visibility.Hidden;
+            return ((CommandViewModel)value).GetType() != null ? Visibility.Visible : Visibility.Hidden;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return ((Command)value).Parameters == null ? Visibility.Hidden : (object)Visibility.Visible;
+            return ((CommandViewModel)value).GetType() == null ? Visibility.Hidden : (object)Visibility.Visible;
         }
     }
 }
