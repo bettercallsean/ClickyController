ee243ae6c32f529caf74f1406498e09b7d774ebf
Sean <seanedwards97@gmail.com>
2020-04-16 18:12:17 +0100

    Create ColumnWidthConverter.cs

---
+++ ClickyControllerGUI/Utilities/ColumnWidthConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace ClickyControllerGUI.Utilities
+{
+    class ColumnWidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string[] parameters = parameter.ToString().Split('|');
+            return int.Parse(parameters[0]) * int.Parse(parameters[1]);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
