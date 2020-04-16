ed52fd9cbc1b880b7c67a0edeb0ce85c47490abe
Sean <seanedwards97@gmail.com>
2020-04-16 18:12:09 +0100

    Add classes that inherit from base Command class

--- ClickyControllerGUI/Models/Command.cs
+++ ClickyControllerGUI/Models/Command.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
+using ClickyController;
 
 namespace ClickyControllerGUI.Models
 {
@@ -11,9 +12,65 @@ namespace ClickyControllerGUI.Models
         {
 
         }
+        
+        public bool TakesParameters { get; set; }
 
-        public string Parameters { get; set; }
         public string Method { get; set; }
 
+    }
+
+    public class MouseClick : Command
+    {
+        public MouseClick()
+        {
+            TakesParameters = false;
+        }
+
+        public string Button { get; set; }
+    }
+
+    public class MouseMove : Command
+    {
+        public MouseMove()
+        {
+            TakesParameters = true;
+        }
+
+        public int XCoords { get; set; }
+        public int YCoords { get; set; }
+        public string Coordinates { get => string.Format("{0}, {1}", XCoords, YCoords); }
+    }
+
+    public class KeyboardCharacterInput : Command
+    {
+        public KeyboardCharacterInput()
+        {
+            TakesParameters = true;
+        }
+
+        // Stores whether the buttons is pressed normally, down or up
+        public string ButtonAction { get; set; }
+
+        public string Character { get; set; }
+    }
+
+    public class KeyboardTextInput : Command
+    {
+        public KeyboardTextInput()
+        {
+            TakesParameters = true;
+        }
+
+        public string Text { get; set; }
+    }
+
+    public class Wait : Command
+    {
+        public Wait()
+        {
+            TakesParameters = true;
+        }
+
+        public int Seconds { get; set; }
     }
 }
