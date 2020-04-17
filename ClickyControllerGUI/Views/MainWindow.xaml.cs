7775d699f931ae6544b4e9bdad9ffd6fd564075e
Sean <seanedwards97@gmail.com>
2020-04-17 21:52:12 +0100

    Remove MouseDoubleClick event method

--- ClickyControllerGUI/Views/MainWindow.xaml.cs
+++ ClickyControllerGUI/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClickyControllerGUI.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,11 +24,6 @@ namespace ClickyControllerGUI
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void ListViewItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-
         }
     }
 }
