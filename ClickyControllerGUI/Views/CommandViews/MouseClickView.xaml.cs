106b9657b62ac8a1efe27cc7bcd92a914b71bf12
Sean <seanedwards97@gmail.com>
2020-04-17 21:53:24 +0100

    Add custom Windows for each CommandViewModel derivative

---
+++ ClickyControllerGUI/Views/CommandViews/MouseClickView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClickyControllerGUI.Views.CommandViews
+{
+    /// <summary>
+    /// Interaction logic for MouseClickView.xaml
+    /// </summary>
+    public partial class MouseClickView : Window
+    {
+        public MouseClickView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ClickyControllerGUI/Views/CommandViews/MouseMoveView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using ClickyControllerGUI.Utilities;
+using ClickyControllerGUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClickyControllerGUI.Views.CommandViews
+{
+    /// <summary>
+    /// Interaction logic for MoveMouseView.xaml
+    /// </summary>
+    public partial class MouseMoveView : Window
+    {
+        public MouseMoveView()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
