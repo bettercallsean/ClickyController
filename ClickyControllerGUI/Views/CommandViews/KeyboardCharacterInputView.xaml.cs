b623ff04982eaa61176abd9738ba1ce7ef360a06
Sean <seanedwards97@gmail.com>
2020-04-18 20:57:53 +0100

    Add event handler when button is pressed

--- ClickyControllerGUI/Views/CommandViews/KeyboardCharacterInputView.xaml.cs
+++ ClickyControllerGUI/Views/CommandViews/KeyboardCharacterInputView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -17,9 +18,29 @@ namespace ClickyControllerGUI.Views.CommandViews
     /// </summary>
     public partial class KeyboardCharacterInputView : Window
     {
+        private readonly Dictionary<string, string> WindowsKeyToButtonDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(ClickyControllerGUI.Resources.WindowsKeysToButton);
         public KeyboardCharacterInputView()
         {
             InitializeComponent();
+        }
+
+        private void KeyPress_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            string character = "";
+            if (string.IsNullOrWhiteSpace(KeyPress.Text))
+                character = KeyPress.Text;
+            
+            
+            if (e.Key.ToString().Length > 1)
+            {
+                e.Handled = true;
+                KeyPress.Text = WindowsKeyToButtonDictionary[e.Key.ToString()];
+            }
+            else
+            {
+                KeyPress.Text = character;
+            }
         }
     }
 }
