70e5586aefc078220559b45467e8b15ff16d7273
Sean <seanedwards97@gmail.com>
2020-05-01 20:26:52 +0100

    Create MouseClickViewModelTests.cs

---
+++ ClickyController.UnitTests/ViewModelsTests/CommandViewModels/MouseClickViewModelTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClickyControllerGUI.ViewModels;
+
+namespace ClickyController.UnitTests.ViewModelsTests
+{
+    [TestClass]
+    public class MouseClickViewModelTests
+    {
+        [TestMethod]
+        public void MouseClickViewModel_SetsCorrectly()
+        {
+            MouseClickViewModel viewModel = new MouseClickViewModel
+            {
+                ViewModel = "MouseClickViewModel"
+            };
+
+            Assert.AreEqual(viewModel.ViewModel, "MouseClickViewModel");
+        }
+
+        [TestMethod]
+        public void MouseClickView_SetsOnInstantiation()
+        {
+            MouseClickViewModel viewModel = new MouseClickViewModel();
+
+            Assert.AreEqual(viewModel.View, "MouseClickView");
+        }
+
+        [TestMethod]
+        public void MouseClickViewModelParameter()
+        {
+            MouseClickViewModel mouseClickViewModel = new MouseClickViewModel
+            {
+                Button = "Left",
+                Action = "Down"
+            };
+
+            Assert.AreEqual(mouseClickViewModel.Parameters, "Left Down");
+        }
+
+    }
+}
