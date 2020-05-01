ea8bffc9c461c41c83befecb56da1811ff276eeb
Sean <seanedwards97@gmail.com>
2020-05-01 19:04:27 +0100

    Update CommandViewModelTests.cs

--- ClickyController.UnitTests/ViewModelsTests/CommandViewModelTests.cs
+++ ClickyController.UnitTests/ViewModelsTests/CommandViewModelTests.cs
@@ -4,67 +4,69 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace ClickyControllerGUI.ViewModels.Tests
+namespace ClickyController.UnitTests.ViewModelsTests
 {
     [TestClass]
     public class CommandViewModelTests
     {
         [TestMethod]
-        public void ViewModel_SetsCorrectly()
+        public void MouseClickViewModel_SetsCorrectly()
         {
-            string viewmodel = "MouseClickViewModel";
-            MouseClickViewModel mouseClickViewModel = new MouseClickViewModel();
-
-            mouseClickViewModel.ViewModel = viewmodel;
-
-            Assert.AreEqual(mouseClickViewModel.ViewModel, viewmodel);
-        }
-
-        [TestMethod]
-        public void View_IsSetOnViewModelInstantiation()
-        {
-            string view = "MouseClickView";
-            MouseClickViewModel mouseClickViewModel = new MouseClickViewModel();
-
-            Assert.AreEqual(mouseClickViewModel.View, view);
-        }
-
-        [TestMethod]
-        public void MouseClickViewModelParameter_Test()
-        {
-            MouseClickViewModel mouseClickViewModel = new MouseClickViewModel
+            MouseClickViewModel viewModel = new MouseClickViewModel
             {
-                Button = "Left",
-                Action = "Down"
+                ViewModel = "MouseClickViewModel"
             };
 
-            Assert.AreEqual(mouseClickViewModel.Parameters, "Left Down");
+            Assert.AreEqual(viewModel.ViewModel, "MouseClickViewModel");
         }
 
         [TestMethod]
-        public void MouseMoveViewModelParameter_Relative_False()
-        { 
-            MouseMoveViewModel viewModel = new MouseMoveViewModel
-            {
-                XCoordinates = 100,
-                YCoordinates = 600,
-                MoveRelative = false
-            };
-
-            Assert.AreEqual(viewModel.Parameters, "100, 600");
-        }
-
-        [TestMethod]
-        public void MouseMoveViewModelParameter_Relative_True()
+        public void MouseMoveViewModel_SetsCorrectly()
         {
             MouseMoveViewModel viewModel = new MouseMoveViewModel
             {
-                XCoordinates = 100,
-                YCoordinates = 600,
-                MoveRelative = true
+                ViewModel = "MouseMoveViewModel"
             };
 
-            Assert.AreEqual(viewModel.Parameters, "Relative: 100, 600");
+            Assert.AreEqual(viewModel.ViewModel, "MouseMoveViewModel");
         }
+
+        [TestMethod]
+        public void KeyboardCharacterInputViewModel_SetsCorrectly()
+        {
+            KeyboardCharacterInputViewModel viewModel = new KeyboardCharacterInputViewModel
+            {
+                ViewModel = "KeyboardCharacterInputViewModel"
+            };
+
+            Assert.AreEqual(viewModel.ViewModel, "KeyboardCharacterInputViewModel");
+        }
+
+        [TestMethod]
+        public void KeyboardTextInputViewModel_SetsCorrectly()
+        {
+            KeyboardTextInputViewModel viewModel = new KeyboardTextInputViewModel
+            {
+                ViewModel = "KeyboardTextInputViewModel"
+            };
+
+            Assert.AreEqual(viewModel.ViewModel, "KeyboardTextInputViewModel");
+        }
+
+        [TestMethod]
+        public void WaitViewModel_SetsCorrectly()
+        {
+            WaitViewModel viewModel = new WaitViewModel
+            {
+                ViewModel = "WaitViewModel"
+            };
+
+            Assert.AreEqual(viewModel.ViewModel, "WaitViewModel");
+        }
+
+
+
+
+
     }
 }
