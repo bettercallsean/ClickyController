34e8ea4d8aa40c021071eb0355885790966c61c0
Sean <seanedwards97@gmail.com>
2020-05-01 19:05:10 +0100

    Create MouseMoveViewModelTests.cs

---
+++ ClickyController.UnitTests/ViewModelsTests/MouseMoveViewModelTests.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClickyControllerGUI.ViewModels;
+
+namespace ClickyController.UnitTests.ViewModelsTests
+{
+    [TestClass]
+    public class MouseMoveViewModelTests
+    {
+        [TestMethod]
+        public void XCoordinate_ValidCoordinate_True()
+        {
+            MouseMoveViewModel viewModel = new MouseMoveViewModel
+            {
+                XCoordinates = 500
+            };
+
+            Assert.IsTrue(viewModel.ValidXCoordinates);
+        }
+
+        [TestMethod]
+        public void YCoordinate_ValidCoordinate_True()
+        {
+            MouseMoveViewModel viewModel = new MouseMoveViewModel
+            {
+                YCoordinates = 500
+            };
+
+            Assert.IsTrue(viewModel.ValidYCoordinates);
+        }
+
+        [TestMethod]
+        public void XCoordinate_ValidCoordinate_True_TryParse()
+        {
+            object value = "200";
+            MouseMoveViewModel viewModel = new MouseMoveViewModel
+            {
+                XCoordinates = (int)value
+            };
+
+            Assert.IsTrue(viewModel.ValidXCoordinates);
+        }
+
+        [TestMethod]
+        public void YCoordinate_ValidCoordinate_True_TryParse()
+        {
+            object value = "200";
+            MouseMoveViewModel viewModel = new MouseMoveViewModel
+            {
+                YCoordinates = (int)value
+            };
+
+            Assert.IsTrue(viewModel.ValidYCoordinates);
+        }
+
+        [TestMethod]
+        public void XCoordinate_ValidCoordinate_False()
+        {
+            object value = "Hello";
+            MouseMoveViewModel viewModel = new MouseMoveViewModel
+            {
+                XCoordinates = (int)value
+            };
+
+            Assert.IsFalse(viewModel.ValidXCoordinates);
+        }
+
+        [TestMethod]
+        public void YCoordinate_ValidCoordinate_False()
+        {
+            object value = "Hello";
+            MouseMoveViewModel viewModel = new MouseMoveViewModel
+            {
+                YCoordinates = (int)value
+            };
+
+            Assert.IsFalse(viewModel.ValidYCoordinates);
+        }
+    }
+}
