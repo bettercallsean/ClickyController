272e8d22632ddf3df0613cfdb1b4cf3699de2502
Sean <seanedwards97@gmail.com>
2020-05-13 21:16:32 +0100

    Fix number virtual codes

--- ClickyController/Properties/Resources.Designer.cs
+++ ClickyController/Properties/Resources.Designer.cs
@@ -61,7 +61,35 @@ namespace ClickyController.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to {&quot;backspace&quot;: 14, &quot;tab&quot;: 15, &quot;clear&quot;: 0, &quot;enter&quot;: 28, &quot;shift&quot;: 42, &quot;ctrl&quot;: 29, &quot;alt&quot;: 56, &quot;pause&quot;: 0, &quot;caps_lock&quot;: 58, &quot;esc&quot;: 1, &quot; &quot;: 57, &quot;page_up&quot;: 73, &quot;page_down&quot;: 81, &quot;end&quot;: 79, &quot;home&quot;: 71, &quot;left_arrow&quot;: 75, &quot;up_arrow&quot;: 72, &quot;right_arrow&quot;: 77, &quot;down_arrow&quot;: 80, &quot;select&quot;: 0, &quot;print&quot;: 0, &quot;execute&quot;: 0, &quot;print_screen&quot;: 84, &quot;ins&quot;: 82, &quot;del&quot;: 83, &quot;help&quot;: 99, &quot;0&quot;: 11, &quot;1&quot;: 2, &quot;2&quot;: 3, &quot;3&quot;: 4, &quot;4&quot;: 5, &quot;5&quot;: 6, &quot;6&quot;: 7, &quot;7&quot;: 8, &quot;8&quot;: 9, &quot;9&quot;: 10, &quot;a&quot;: 30, &quot;b&quot;: 48, &quot;c&quot;: 46, &quot;d&quot;: 32, &quot;e&quot;: 18, &quot;f&quot;: 33, &quot;g&quot;: 34, &quot;h&quot;: 35, &quot; [rest of string was truncated]&quot;;.
+        ///   Looks up a localized string similar to {
+        ///    &quot;backspace&quot;: 14, 
+        ///    &quot;tab&quot;: 15, 
+        ///    &quot;clear&quot;: 0, 
+        ///    &quot;enter&quot;: 28, 
+        ///    &quot;return&quot;: 28, 
+        ///    &quot;shift&quot;: 42, 
+        ///    &quot;ctrl&quot;: 29, 
+        ///    &quot;alt&quot;: 56, 
+        ///    &quot;pause&quot;: 0, 
+        ///    &quot;capslock&quot;: 58, 
+        ///    &quot;esc&quot;: 1,
+        ///    &quot; &quot;: 57, 
+        ///    &quot;space&quot;: 57, 
+        ///    &quot;pageup&quot;: 73, 
+        ///    &quot;pagedown&quot;: 81, 
+        ///    &quot;end&quot;: 79, 
+        ///    &quot;home&quot;: 71, 
+        ///    &quot;left&quot;: 75, 
+        ///    &quot;up&quot;: 72, 
+        ///    &quot;right&quot;: 77, 
+        ///    &quot;down&quot;: 80, 
+        ///    &quot;select&quot;: 0, 
+        ///    &quot;print&quot;: 0, 
+        ///    &quot;execute&quot;: 0, 
+        ///    &quot;printscreen&quot;: 84,
+        ///    &quot;prtscn&quot;: 84, 
+        ///    &quot;ins&quot;: 82, 
+        ///    [rest of string was truncated]&quot;;.
         /// </summary>
         public static string ScanCodes {
             get {
@@ -70,7 +98,34 @@ namespace ClickyController.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to {&quot;backspace&quot;: 8, &quot;tab&quot;: 9, &quot;clear&quot;: 254, &quot;enter&quot;: 13, &quot;shift&quot;: 16, &quot;ctrl&quot;: 17, &quot;alt&quot;: 18, &quot;pause&quot;: 19, &quot;caps_lock&quot;: 20, &quot;esc&quot;: 27, &quot; &quot;: 32, &quot;page_up&quot;: 33, &quot;page_down&quot;: 34, &quot;end&quot;: 35, &quot;home&quot;: 36, &quot;left_arrow&quot;: 37, &quot;up_arrow&quot;: 38, &quot;right_arrow&quot;: 39, &quot;down_arrow&quot;: 40, &quot;select&quot;: 41, &quot;print&quot;: 42, &quot;execute&quot;: 43, &quot;print_screen&quot;: 44, &quot;ins&quot;: 45, &quot;del&quot;: 46, &quot;help&quot;: 47, &quot;0&quot;: 48, &quot;1&quot;: 49, &quot;2&quot;: 50, &quot;3&quot;: 51, &quot;4&quot;: 52, &quot;5&quot;: 53, &quot;6&quot;: 54, &quot;7&quot;: 55, &quot;8&quot;: 56, &quot;9&quot;: 57, &quot;a&quot;: 65, &quot;b&quot;: 66, &quot;c&quot;: 67, &quot;d&quot;: 68, &quot;e&quot;: 69, &quot;f&quot;: 70, &quot;g&quot;: 7 [rest of string was truncated]&quot;;.
+        ///   Looks up a localized string similar to {
+        ///    &quot;backspace&quot; : 8,
+        ///    &quot;tab&quot; : 9,
+        ///    &quot;clear&quot; : 254,
+        ///    &quot;enter&quot; : 13,
+        ///    &quot;return&quot; : 13,
+        ///    &quot;shift&quot; : 16,
+        ///    &quot;ctrl&quot; : 17,
+        ///    &quot;alt&quot; : 18,
+        ///    &quot;pause&quot; : 19,
+        ///    &quot;capslock&quot; : 20,
+        ///    &quot;esc&quot; : 27,
+        ///    &quot;space&quot; : 32,
+        ///    &quot; &quot; : 32,
+        ///    &quot;pageup&quot; : 33,
+        ///    &quot;pagedown&quot; : 34,
+        ///    &quot;end&quot; : 35,
+        ///    &quot;home&quot; : 36,
+        ///    &quot;left&quot; : 37,
+        ///    &quot;up&quot; : 38,
+        ///    &quot;right&quot; : 39,
+        ///    &quot;down&quot; : 40,
+        ///    &quot;select&quot; : 41,
+        ///    &quot;print&quot; : 42,
+        ///    &quot;execute&quot; : 43,
+        ///    &quot;printscreen&quot; : 44,
+        ///    &quot;prtscn&quot; : 44,
+        ///    &quot;ins&quot; : 4 [rest of string was truncated]&quot;;.
         /// </summary>
         public static string VirtualKeyCodes {
             get {
